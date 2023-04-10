35fcd9743382a054799b63da1f171dd3e89a8bdb
macross0 <maxnekuga@gmail.com>
2023-04-10 16:21:39 +0300

    Rewrite CardViews generation

--- app/src/main/java/com/example/gradework/NoteEdit.java
+++ app/src/main/java/com/example/gradework/NoteEdit.java
@@ -1,11 +1,20 @@
 package com.example.gradework;
 
+import static android.content.ContentValues.TAG;
+
+import static java.util.Calendar.SHORT_FORMAT;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
@@ -17,14 +26,26 @@ import android.widget.TimePicker;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class NoteEdit extends AppCompatActivity {
 
     Toolbar toolbar;
+    Calendar date;
+    Note note;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        note = (Note) intent.getSerializableExtra("note");
         setContentView(R.layout.note_edit);
 
         toolbar = findViewById(R.id.noteEditToolbar);
@@ -32,21 +53,60 @@ public class NoteEdit extends AppCompatActivity {
         toolbar.setTitle("Редагування нотатки");
         setSupportActionBar(toolbar);
 
+        TextInputEditText topicInput = findViewById(R.id.noteTopicInput);
+        TextInputEditText textInput = findViewById(R.id.noteTextInput);
+        TextView notificationTime = findViewById(R.id.time);
         Switch enableSwitch = findViewById(R.id.enableEditSwitch);
-        CalendarView calendar = findViewById(R.id.calendar);
-        TimePicker timePicker = findViewById(R.id.timePicker);
-        calendar.setVisibility(View.GONE);
-        timePicker.setVisibility(View.GONE);
+        Button setTimeButton = findViewById(R.id.setTimeButton);
+
+        if (note.topic.equals("")) {
+            topicInput.setText("");
+        } else {
+            topicInput.setText(note.topic);
+        }
+        textInput.setText(note.text);
+        if (note.notificationTimestamp != 0) {
+            Date datetime = new Date(note.notificationTimestamp);
+            notificationTime.setText((String) android.text.format.DateFormat.format("dd.MM.yy HH:mm", datetime));
+        } else {
+            notificationTime.setText("Час не вказано");
+        }
+
+        setTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                showDateTimePicker();
+            }
+        });
         enableSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    calendar.setVisibility(View.VISIBLE);
-                    timePicker.setVisibility(View.VISIBLE);
+
                 } else {
-                    calendar.setVisibility(View.GONE);
-                    timePicker.setVisibility(View.GONE);
+
                 }
             }
         });
+    }
+
+    public void showDateTimePicker() {
+        final Calendar currentDate = Calendar.getInstance();
+        date = Calendar.getInstance();
+        new DatePickerDialog(NoteEdit.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                date.set(year, monthOfYear, dayOfMonth);
+                new TimePickerDialog(NoteEdit.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        date.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        date.set(Calendar.MINUTE, minute);
+                        note.notificationTimestamp = date.getTimeInMillis();
+                        TextView time = findViewById(R.id.time);
+                        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+                        time.setText(df.format(date.getTime()));
+                    }
+                }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), true).show();
+            }
+        }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
     }
 }
--- app/src/main/java/com/example/gradework/NotesList.java
+++ app/src/main/java/com/example/gradework/NotesList.java
@@ -1,7 +1,13 @@
 package com.example.gradework;
 
+import static android.view.View.TEXT_ALIGNMENT_CENTER;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
@@ -9,22 +15,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.text.TextUtils;
+import android.util.ArrayMap;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Switch;
+import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.android.material.divider.MaterialDivider;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class NotesList extends AppCompatActivity {
 
     public ScrollView notesScrollView;
     public LinearLayout notesLayout;
-    List<Note> notes = new ArrayList<Note>();
+    Map<Integer, Note> notes = new ArrayMap<Integer, Note>();
 
     LayoutInflater inflater;
 
@@ -34,47 +52,43 @@ public class NotesList extends AppCompatActivity {
         setContentView(R.layout.notes_list);
         setTitle(R.string.app_name);
 
-        inflater = (LayoutInflater) getApplicationContext().getSystemService
-                (Context.LAYOUT_INFLATER_SERVICE);
         notesLayout = findViewById(R.id.notesLinearLayout);
 
-        for (int i = 0; i < 100; i++) {
-            Note note = new Note(getApplicationContext());
-            if (i % 2 == 0) {
-                note.topic = String.format("topic %d", i);
-            }
-            note.text = String.format("%d", i);
-            note.write();
-        }
+//        for (int i = 0; i < 100; i++) {
+//            Note note = new Note(getApplicationContext());
+//            if (i % 2 == 0) {
+//                note.topic = String.format("topic %d", i);
+//            }
+//            note.text = String.format("%d", i);
+//            note.write();
+//        }
 
         String[] fileNames = fileList();
-        for (String fileName : fileNames) {
-            Note note = new Note(getApplicationContext(), fileName);
+        for (int i = 0; i < fileNames.length; i++) {
+            Note note = new Note(getApplicationContext(), fileNames[i]);
             note.read();
-//            notes.add(note);
 
-            View card = inflater.inflate(R.layout.note_card,null);
+            CardView card = generateNoteView(NotesList.this, note);
             notesLayout.addView(card);
-            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) card.getLayoutParams();
-            params.setMargins(0, 0, 0, this.spToPx(8));
-            card.setLayoutParams(params);
-
-            TextView description = card.findViewById(R.id.description);
-            description.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), NoteEdit.class);
-                    startActivity(intent);
-                }
-            });
-            TextView datetime = card.findViewById(R.id.notificationDatetime);
-//            Switch enable_switch = card.findViewById(R.id.enableSwitch);
-
-            description.setText(note.getDescription());
-            if (note.hasTopic()) {
-                description.setTypeface(null, Typeface.BOLD);
-            }
-            datetime.setText(note.getModificationDateTime());
         }
+    }
+//            description.setOnClickListener(new View.OnClickListener() {
+//                @Override public void onClick(View v) {
+//                    Bundle include = new Bundle();
+//                    Intent intent = new Intent(getApplicationContext(), NoteEdit.class);
+//                    intent.putExtra("note", finalNote);
+//                    startActivity(intent);
+//                }
+//            });
+//            TextView datetime = card.findViewById(R.id.notificationDatetime);
+////            Switch enable_switch = card.findViewById(R.id.enableSwitch);
+//
+//            description.setText(note.getDescription());
+//            if (note.hasTopic()) {
+//                description.setTypeface(null, Typeface.BOLD);
+//            }
+//            datetime.setText(note.getModificationDateTime());
+//        }
 
 //        fileText = findViewById(R.id.read_data);
 //        writeButton = findViewById(R.id.write);
@@ -92,6 +106,105 @@ public class NotesList extends AppCompatActivity {
 ////                fileText.setText();
 //            }
 //        });
+//    }
+
+    private CardView generateNoteView(Context context, Note note) {
+        TextView description = new TextView(context);
+        if (note.hasTopic()) {
+            description.setText(note.topic);
+            description.setTypeface(null, Typeface.BOLD);
+        } else {
+            description.setText(note.text);
+        }
+        LinearLayout.LayoutParams descriptionParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                9.0f
+        );
+        descriptionParams.setMargins(0, 0, this.spToPx(10), 0);
+        description.setLayoutParams(descriptionParams);
+        description.setClickable(true);
+        description.setEllipsize(TextUtils.TruncateAt.END);
+        description.setGravity(Gravity.CENTER_VERTICAL);
+        description.setSingleLine(false);
+        description.setTextColor(getColor(R.color.white));
+        description.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Bundle include = new Bundle();
+                Intent intent = new Intent(getApplicationContext(), NoteEdit.class);
+                intent.putExtra("note", note);
+                startActivity(intent);
+            }
+        });
+
+        View divider = new View(context);
+        LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(
+                1,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        dividerParams.setMargins(0, 0, this.spToPx(10), 0);
+        divider.setLayoutParams(dividerParams);
+        divider.setBackgroundColor(getColor(R.color.white));
+
+        TextView notificationDatetime = new TextView(context);
+        LinearLayout.LayoutParams datetimeParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        datetimeParams.setMargins(0, 0, this.spToPx(10), 0);
+        notificationDatetime.setLayoutParams(datetimeParams);
+        Date datetime = new Date(note.notificationTimestamp);
+        notificationDatetime.setText((String) android.text.format.DateFormat.format("dd.MM.yy\nHH:mm", datetime));
+        notificationDatetime.setTextColor(getColor(R.color.white));
+        notificationDatetime.setGravity(Gravity.CENTER_VERTICAL);
+        notificationDatetime.setPadding(0, 0, 0, 0);
+        notificationDatetime.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+
+        Switch enableSwitch = new Switch(context);
+        LinearLayout.LayoutParams switchParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        switchParams.gravity = Gravity.CENTER;
+        enableSwitch.setLayoutParams(switchParams);
+        enableSwitch.setPadding(0, this.spToPx(8), 0, this.spToPx(8));
+
+        LinearLayout views = new LinearLayout(context);
+        LinearLayout.LayoutParams rowParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        rowParams.setMargins(
+                this.spToPx(16),
+                this.spToPx(12),
+                this.spToPx(12),
+                this.spToPx(12)
+        );
+        views.setLayoutParams(rowParams);
+        views.setOrientation(LinearLayout.HORIZONTAL);
+        views.addView(description);
+        views.addView(divider);
+        views.addView(notificationDatetime);
+        views.addView(enableSwitch);
+
+        CardView card = new CardView(context);
+        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        cardParams.setMargins(
+                this.spToPx(8),
+                this.spToPx(4),
+                this.spToPx(8),
+                this.spToPx(4)
+        );
+        card.setLayoutParams(cardParams);
+        card.setCardBackgroundColor(getColor(androidx.cardview.R.color.cardview_dark_background));
+        card.setClickable(true);
+        card.setRadius(this.spToPx(8));
+        card.addView(views);
+
+        return card;
     }
 
     private int spToPx(float sp) {
